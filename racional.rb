12cd10836717a4673905c08bfd4332cd5079cac1
Daura Hernández <alu0100600643@ull.edu.es>
2013-10-18 12:48:19 +0100

    mi practica 5

--- racional.rb
+++ racional.rb
@@ -3,4 +3,79 @@
 require "./gcd.rb"
 
 class Fraccion
+
+  #getters de la clase
+  attr_accessor :num, :denom
+
+  #método para inicializar la clase 
+ def initialize(n1, n2)
+    @num = n1
+    @denom = n2
+
+ end
+  
+ def to_s 
+    
+    "#{@num}/#{@denom}"
+    
+ end
+  
+  
+ def +(objeto)
+    
+    minim = minimo(@denom, objeto.denom)
+    miFraccion = minim/@denom 
+    otraFraccion = minim/objeto.denom
+  
+    miFraccion2 = miFraccion  * @num
+    otraFraccion2 = otraFraccion * objeto.num
+   
+    numerador = miFraccion2 + otraFraccion2
+    mcd=gcd(numerador,minim)
+    Fraccion.new(numerador/mcd , minim/mcd )
+    
+    
+ end
+  
+ def minimo(a, b)
+     
+     min = (a*b)/gcd(a,b)
+
+     
+ end
+    
+    
+    # devuelve un nuevo racional que resta al objeto que invoca el que le pasan como parámetro
+ def -(objeto)                       
+   min = minimo(@denom, objeto.denom)
+   miFraccion = min/@denom 
+   otraFraccion = min/objeto.denom
+  
+   miFraccion2 = miFraccion  * @num
+   otraFraccion2 = otraFraccion * objeto.num
+   
+   numerador = miFraccion2 - otraFraccion2
+   mcd = gcd(numerador, min)
+   Fraccion.new(numerador/mcd , min/mcd )   
+    
+ end
+   # devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+ def * (objeto)
+     num1 = @num * objeto.num
+     denom1 = @denom * objeto.denom
+     mcd = gcd(num1, denom1)
+     Fraccion.new(num1/mcd, denom1/mcd)
+    
+ end
+  
+ def / (objeto)
+    num1 = @num * objeto.denom
+    denom1 = @denom * objeto.num
+    mcd = gcd( num1, denom1)
+     Fraccion.new(num1/mcd, denom1/mcd)
+ end
+
 end
+
+
+
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,12 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+  
+          def test_1
+                assert_equal "1/1", (Fraccion.new(3,4) + Fraccion.new(1,4)).to_s      
+                assert_equal "1/2", (Fraccion.new(4,4) - Fraccion.new(2,4)).to_s
+                assert_equal "1/2", (Fraccion.new(1,4) * Fraccion.new(2,1)).to_s
+                assert_equal "1/2", (Fraccion.new(1,4) / Fraccion.new(1,2)).to_s
+        end
+  
 end
